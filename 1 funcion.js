a757aff69d127f47534b7e25c822c3df701a8286
Oscar Ayala <titoayala@gmail.com>
2022-06-25 10:14:22 -0400

    Subiendo estructura del desafío

---
+++ 1 funcion.js
@@ -0,0 +1,13 @@
+/* FUNCION STANDARD CON PARAMETROS
+function example(a, b, c) {
+    return a + b + c;
+    
+} */
+
+//TRANSFORMACION A EXPRESION DE FUNCION CON PARAMETROS
+const example = function (a, b, c) {
+    return a + b + c;
+}
+
+//LLAMADO A LA FUNCION
+resultado = example(3,4,3);
---
+++ 2 funcion.js
@@ -0,0 +1,12 @@
+/* EXPRESION DE FUNCION CON PARAMETROS
+suma = function (a, b) {
+    return a + b;
+} */
+
+//TRANSFORMACION A FUNCION FLECHA CON PARAMETROS
+const suma = (a, b) => { 
+    return a + b; 
+}
+
+//LLAMADO A LA FUNCION
+resultado = suma(1,2);
